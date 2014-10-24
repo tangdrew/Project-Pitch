496bb4b9f83039fcfa61cb1f646f3b6e7d334434
Andrew Tang <andrewtang@sbcglobal.net>
2014-10-24 16:24:34 -0500

    hide

--- js/locus.js
+++ js/locus.js
@@ -2,12 +2,12 @@ function show(type){
 	console.log('works');
 	if(type == "investor"){
 		console.log('investor');
-		document.getElementById('businessForm').style.display = "inline";
+		document.getElementById('businessForm').style.display = "none";
 		document.getElementById('investorForm').style.display = "normal";
 	}
 	if(type == "business"){
 		console.log('business');
-		document.getElementById('investorForm').style.display = "inline";
+		document.getElementById('investorForm').style.display = "none";
 		document.getElementById('businessForm').style.display = "normal";
 	}
 }
