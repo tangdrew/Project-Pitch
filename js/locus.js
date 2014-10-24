6bc8b15b42a9740c2445878c1d02735e0dde6302
Andrew Tang <andrewtang@sbcglobal.net>
2014-10-24 16:21:11 -0500

    debug

--- js/locus.js
+++ js/locus.js
@@ -1,9 +1,12 @@
 function show(type){
+	console.log('works');
 	if(type == "investor"){
+		console.log('investor');
 		document.getElementById('businessForm').display = "inline";
 		document.getElementById('investorForm').display = "normal";
 	}
 	if(type == "business"){
+		console.log('business');
 		document.getElementById('investorForm').display = "inline";
 		document.getElementById('businessForm').display = "normal";
 	}
